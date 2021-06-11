1998be7daec1d0aa2082703effec1cdd3ad877c6
dhruv17sep <dhruv17sep@gmail.com>
2021-06-11 10:16:52 +0000

    Commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/ovp/app/Component'], function(AppComponent) {
+    return AppComponent.extend("ovpcard.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
